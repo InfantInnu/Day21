a848a5b988e34e0eef5528334be7e5bca3d97c4d
InfantInnu <96235043+InfantInnu@users.noreply.github.com>
2022-01-23 19:05:44 +0530

    Add files via upload

---
+++ call back/callbackhell.js
@@ -0,0 +1,42 @@
+var wishes=document.createElement("div");
+wishes.style.height="60rem";
+wishes.style.width="60rem";
+wishes.style.color="blue";
+wishes.style.textalign="center";
+setTimeout(()=>{
+    wishes.innerText="Let's Begin";
+},1000);
+setTimeout(()=>{
+    wishes.innerText="10";
+  },2000);
+  setTimeout(()=>{
+    wishes.innerText="9"; 
+},3000);  
+setTimeout(()=>{
+    wishes.innerText="8"; 
+},4000);  
+setTimeout(()=>{
+    wishes.innerText="7"; 
+},5000);  
+setTimeout(()=>{
+    wishes.innerText="6"; 
+},6000);  
+setTimeout(()=>{
+    wishes.innerText="5"; 
+},7000);  
+setTimeout(()=>{
+    wishes.innerText="4"; 
+},8000); 
+setTimeout(()=>{
+    wishes.innerText="3"; 
+},9000); 
+setTimeout(()=>{
+    wishes.innerText="2"; 
+},9000); 
+setTimeout(()=>{
+    wishes.innerText="1"; 
+},10_000); 
+setTimeout(()=>{
+    wishes.innerText="Happy Birthday";
+},12_000);
+document.body.appendChild(wishes);
